df7cbac849774f77ba7e94d8c3abde3b7953b55e
Kk Shinkai <kkshinkai@kkshinkai.moe>
2022-04-10 18:04:33 +0800

    Add input character stream

--- main.rkt
+++ main.rkt
@@ -1,1 +1,29 @@
 #lang racket
+
+(struct istream (src [pos #:mutable] [line #:mutable] [column #:mutable]))
+
+(define (istream-make src)
+  (istream src 0 1 0))
+
+(define (istream-eof? is)
+  (>= (istream-pos is) (string-length (istream-src is))))
+
+(define (istream-next is)
+  (if (istream-eof? is)
+      (raise "reached end of stream unexpectedly")
+      (let ([c (string-ref (istream-src is) (istream-pos is))])
+        (set-istream-pos! is (add1 (istream-pos is)))
+        (case c
+          [(#\newline) (begin
+                         (set-istream-line! is (add1 (istream-line is)))
+                         (set-istream-column! is 0))]
+          [else (set-istream-column! is (add1 (istream-column is)))])
+        c)))
+
+(define (istream-peek is)
+  (string-ref (istream-src is) (istream-pos is)))
+
+(define (istream-diagnosis is message)
+  (displayln
+    (format "error: ~a at line: ~a, column ~a"
+      message (istream-line is) (istream-column is))))
