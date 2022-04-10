9c75884e91e473a742b3664acdfffea35c28bbf0
Kk Shinkai <kkshinkai@kkshinkai.moe>
2022-04-10 21:15:46 +0800

    Add lexer for number

--- main.rkt
+++ main.rkt
@@ -27,3 +27,42 @@
   (displayln
     (format "error: ~a at line: ~a, column ~a"
       message (istream-line is) (istream-column is))))
+
+(struct token (kind value))
+
+(define token-make
+  (case-lambda [(kind) (token kind #f)]
+               [(kind value) (token kind value)]))
+
+(define (char-ident-hd? c)
+  (or (char-alphabetic? c) (char=? c #\_)))
+(define (char-ident-tl? c)
+  (or (char-ident-hd? c) (char-numeric? c)))
+(define (char-operator? c)
+  (member c (string->list "~!@#$%^&*-+=|?<>./")))
+
+(struct lexer (istream [tokens #:mutable]))
+
+(define (lexer-make src)
+  (lexer (istream-make src) #()))
+
+(define (lexer-read-while lexer pred)
+  (let loop ([src ""])
+    (if (pred (istream-peek (lexer-istream lexer)))
+        (loop (string-append src (string (istream-next (lexer-istream lexer)))))
+        src)))
+
+(define (lexer-read-number lexer)
+  (let loop ([has-dot? #f]
+             [number ""])
+    (if (istream-eof? (lexer-istream lexer))
+        (token-make 'number (string->number number))
+        (cond
+          [(char=? #\. (istream-peek (lexer-istream lexer)))
+           (if has-dot?
+               (token-make 'number (string->number number))
+               (loop #t (string-append number (string (istream-next (lexer-istream lexer))))))]
+          [(char-numeric? (istream-peek (lexer-istream lexer)))
+           (loop has-dot?
+                 (string-append number (string (istream-next (lexer-istream lexer)))))]
+          [else (token-make 'number (string->number number))]))))
